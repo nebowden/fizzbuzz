25121a4754d1343a52d0479d82b33c532f9dd11a
nebowden <nathanebowden@gmail.com>
2014-11-18 15:14:56 -0600

    created html and js docs, drafted basic js to perform calculations, added button to start

---
+++ js/app.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+	$('button').click(function() {
+		for (var i=1; i<101; i++) {
+			if ((i%3)==0 && (i%5)==0) {
+				$('body').append("fizz buzz");
+			}
+			else if ((i%3)==0) {
+				$('body').append("fizz");			
+			}
+			else if ((i%5)==0) {
+				$('body').append("buzz");
+			}
+			else {
+				$('body').append(i);	
+			};
+			$('body').append("<br>");
+		};
+	});
+})
