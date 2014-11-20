214a47de6c9e0070bdcd7b4f81596b73802aa312
nebowden <nathanebowden@gmail.com>
2014-11-20 11:27:44 -0600

    created trigger for keydown on return, changed form clear to apply on click or focus and after entering incorrect value

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
 
-	$('button').click(function() {
+	var mainFunction = function() {
 		var userNumber = +$('.user-number').val();
-		var alertMessage = "Please enter a whole numeral greater than 0."
+		var alertMessage = "Please enter a whole numeral greater than 0.";
 		var runFizz = function (limit){
 			for (var i=1; i <= limit; i++) {
 				if ((i%3)==0 && (i%5)==0) {
@@ -22,24 +22,41 @@ $(document).ready(function() {
 
 		if (isNaN(userNumber)) {
 			alert(alertMessage);
+			clearField();
 		}
 
 		else if (userNumber <= 0) {
 			alert(alertMessage);
+			clearField();
 		}
 
 		else if ((userNumber%1) != 0) {
 			alert(alertMessage);	
+			clearField();
 		}
 
 		else { 
 
 			runFizz(userNumber);
+			$(".user-number").blur();
 		};
+	};
+
+	$('.start-button').click(function(){
+		mainFunction()});
+
+	$('.user-number').keydown(function (event){
+		if(event.which==13)
+		{
+		mainFunction()};
 	});
 
-	$(".user-number").focus(function () {
-			$(this).val('');				
+	var clearField = function () {			
 			$(".fb-list").find("li").remove();
+			$(".user-number").val('');	
+			};
+
+	$(".user-number").on("click focus", function () {
+			clearField();
 			});
 })
