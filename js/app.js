4c00d6c4dd8d6599d31f3f9c2fc2e0d447de5f79
nebowden <nathanebowden@gmail.com>
2014-11-20 09:02:20 -0600

    added text input, created function to run with input as argument, added reset of list as js, made list a ul in html

--- js/app.js
+++ js/app.js
@@ -1,19 +1,45 @@
 $(document).ready(function() {
+
 	$('button').click(function() {
-		for (var i=1; i<101; i++) {
-			if ((i%3)==0 && (i%5)==0) {
-				$('body').append("fizz buzz");
-			}
-			else if ((i%3)==0) {
-				$('body').append("fizz");			
-			}
-			else if ((i%5)==0) {
-				$('body').append("buzz");
-			}
-			else {
-				$('body').append(i);	
+		var userNumber = +$('.user-number').val();
+		var alertMessage = "Please enter a whole numeral greater than 0."
+		var runFizz = function (limit){
+			for (var i=1; i <= limit; i++) {
+				if ((i%3)==0 && (i%5)==0) {
+					$('.fb-list').append("<li>fizz buzz</li>");
+				}
+				else if ((i%3)==0) {
+					$('.fb-list').append("<li>fizz</li>");			
+				}
+				else if ((i%5)==0) {
+					$('fb-list').append("<li>buzz</li>");
+				}
+				else {
+					$('.fb-list').append("<li>" + i + "</li>");	
+				};
 			};
-			$('body').append("<br>");
+		};
+
+		if (isNaN(userNumber)) {
+			alert(alertMessage);
+		}
+
+		else if (userNumber <= 0) {
+			alert(alertMessage);
+		}
+
+		else if ((userNumber%1) != 0) {
+			alert(alertMessage);	
+		}
+
+		else { 
+
+			runFizz(userNumber);
 		};
 	});
+
+	$(".user-number").focus(function () {
+			$(this).val('');				
+			$(".fb-list").find("li").remove();
+			});
 })
